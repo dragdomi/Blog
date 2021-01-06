c3b598e8f7185ebae2236a790ec31ac298a94699
dragdominique <dominikdrag98@gmail.com>
2021-01-06 21:05:05 +0100

    Add bootstrap

--- domiblog/blogapp/urls.py
+++ domiblog/blogapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import HomeView, PostDetailsView
 
 urlpatterns = [
-    path('', views.home, name = "home"),
+    # path('', views.home, name = "home"),
+    path('', HomeView.as_view(), name="home"),
+    path('post/<int:pk>', PostDetailsView.as_view(), name="post_details")
 ]
