4170b2de17a641d48285f414ca86bd8b60bf4525
dragdominique <dominikdrag98@gmail.com>
2021-01-13 22:33:34 +0100

    Remove comment

--- domiblog/blogapp/models.py
+++ domiblog/blogapp/models.py
@@ -19,7 +19,6 @@ class Post(models.Model):
     title_tag = models.CharField(max_length=255, default="")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = RichTextField(blank=True, null=True)
-    # body = models.TextField()
     publication_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default="coding")
     likes = models.ManyToManyField(User, related_name='blog_posts')
