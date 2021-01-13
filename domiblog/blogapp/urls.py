658aa0f9bf67ab504fb31f7c552c7494de3bf5ae
dragdominique <dominikdrag98@gmail.com>
2021-01-13 21:23:09 +0100

    Add likes

--- domiblog/blogapp/urls.py
+++ domiblog/blogapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, PostDetailsView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView
+from .views import HomeView, PostDetailsView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView, LikeView
 
 urlpatterns = [
     # path('', views.home, name = "home"),
@@ -11,5 +11,6 @@ urlpatterns = [
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
     path('article/<int:pk>/remove', DeletePostView.as_view(), name="delete_post"),
     path('category/<str:categories>/', CategoryView, name="category"),
-    path('category-list/', CategoryListView, name="category-list")
+    path('category-list/', CategoryListView, name="category-list"),
+    path('like/<int:pk>', LikeView, name='like_post')
 ]
--- domiblog/blogapp/views.py
+++ domiblog/blogapp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Category
 from .forms import PostForm, EditForm
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
 
 # def home(request):
 #     return render(request, "home.html", {})
@@ -10,24 +11,36 @@ from django.urls import reverse_lazy
 
 class HomeView(ListView):
     model = Post
-    template_name = "home.html"
+    template_name = 'home.html'
     ordering = ['publication_date']
 
     def get_context_data(self, *args, **kwargs):
         categories_menu = Category.objects.all()
         context = super(HomeView, self).get_context_data()
-        context["categories_menu"] = categories_menu
         return context
+
+
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    liked = False
+
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+    return HttpResponseRedirect(reverse('post_details', args=[str(pk)]))
 
 
 def CategoryListView(request):
     category_menu_list = Category.objects.all()
-    return render(request, "category_list.html", {'category_menu_list': category_menu_list})
+    return render(request, 'category_list.html', {'category_menu_list': category_menu_list})
 
 
 def CategoryView(request, categories):
     category_posts = Post.objects.filter(category=categories.replace('-', ' '))
-    return render(request, "categories.html", {'categories': categories.title().replace('-', ' '), 'category_posts': category_posts})
+    return render(request, 'categories.html', {'categories': categories.title().replace('-', ' '), 'category_posts': category_posts})
 
 
 class PostDetailsView(DetailView):
@@ -37,7 +50,16 @@ class PostDetailsView(DetailView):
     def get_context_data(self, *args, **kwargs):
         categories_menu = Category.objects.all()
         context = super(PostDetailsView, self).get_context_data()
-        context["categories_menu"] = categories_menu
+        post = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = post.total_likes()
+        liked = False
+
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        context['categories_menu'] = categories_menu
+        context['total_likes'] = total_likes
+        context['liked'] = liked
         return context
 
 
