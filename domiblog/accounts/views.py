a30aa2253620490a9ead7556b83ab06c4ebd4e47
dragdominique <dominikdrag98@gmail.com>
2021-01-14 22:45:37 +0100

    Add edit profile, password change

--- domiblog/accounts/views.py
+++ domiblog/accounts/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import RegisterForm
+from .forms import RegisterForm, EditProfileForm, PasswordChangeForm
 
 
 class UserRegisterView(generic.CreateView):
     form_class = RegisterForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
+
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('password_success')
+
+
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
